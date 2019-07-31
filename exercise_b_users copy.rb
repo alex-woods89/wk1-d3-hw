41c08a1253b2778c410767b2a0fb60d43cdfb7c2
Alex Woods <alex.woods89@hotmail.co.uk>
2019-07-31 16:23:19 +0100

    1st draft

--- exercise_b_users copy.rb
+++ exercise_b_users copy.rb
@@ -55,12 +55,21 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users ["Jonathan"][:twitter]
 # 2. Get Erik's hometown
+p users ["Erik"][:home_town]
 # 3. Get the array of Erik's lottery numbers
+p users ["Erik"][:lottery_numbers]
 # 4. Get the type of Avril's pet Monty
+# p users ["Avril"][:pets][:species]
 # 5. Get the smallest of Erik's lottery numbers
+p users ["Erik"][:lottery_numbers].sort.first
 # 6. Return an array of Avril's lottery numbers that are even
+# p users ["Avril"][:lottery_numbers]
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users ["Erik"][:lottery_numbers].push(7)
 # 8. Change Erik's hometown to Edinburgh
+users ["Erik"][:home_town => "Edinburgh"]
 # 9. Add a pet dog to Erik called "Fluffy"
+users ["Erik"][:pets{:name => "Fluffy", :species = "Dog"}]
 # 10. Add another person to the users hash
--- exercise_c_uk copy.rb
+++ exercise_c_uk copy.rb
@@ -7,16 +7,30 @@ united_kingdom = [
   {
     name: "Wales",
     population: 3063000,
-    capital: "Swansea"
+    capital: "Cardiff"
   },
   {
     name: "England",
     population: 53010000,
     capital: "London"
+  },
+  {
+    name: "Northern Ireland",
+    population: 1811000,
+    capital: "Belfast"
   }
 ]
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
 # 3. Use a loop to print the names of all the countries in the UK.
+for country in united_kingdom
+  p country[:name]
+end
 # 4. Use a loop to find the total population of the UK.
+
+total_population = 0
+for population in united_kingdom
+  total_population += population[:population]
+  p total_population
+  end
